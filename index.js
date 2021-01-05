2a7eee94500e1dbdc77b51e3157ffff8923add24
Jordan Weatherford <jrw821@gmail.com>
2021-01-05 12:12:38 -0800

    added front end build

--- index.js
+++ index.js
@@ -9,16 +9,27 @@ const { PORT } = require('./variables')
 
 // connect to database
 const mongoose = require('mongoose')
-const DB_URL = require('./variables').DB_URL
+const { DB_URL } = require('./variables')
+
+
+
+const path = __dirname + '/views/'
 
 
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    app.use(express.static(path))
+    app.get('/', function (req,res) {
+        res.sendFile(path + "index.html");
+      });
+
+
+
+    app.use(cors({origin: '*'}))
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({extended : true}))
-    app.use(cors())
     app.use('/api', apiRoutes)
     app.use('/', (req, res) => {
-        res.send('Goods API')
+        res.send("You shouldn't be here. Error!")
     })
     app.listen(PORT)    
 })

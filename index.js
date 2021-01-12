4ba2265a8f0373d5989b52f0fab58a5e265e80ff
Jordan Weatherford <jrw821@gmail.com>
2021-01-12 07:55:26 -0800

    Troubleshooting heroku env variables

--- index.js
+++ index.js
@@ -4,26 +4,31 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const apiRoutes = require('./routes/api')
-const { PORT } = require('./variables')
-
 
 // connect to database
 const mongoose = require('mongoose')
-const { DB_URL } = require('./variables')
+const { DB_URL, PORT } = require('./variables')
 
 console.log("DB URL === ", DB_URL)
 
-
-mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    app.use(cors({origin: '*'}))
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({extended : true}))
-    app.use('/api', apiRoutes)
-
-
-
-    app.use('/', (req, res) => {
-        res.send({"success": false, "message": 'catchall route'})
-    })
-    app.listen(PORT)    
+app.use(cors({origin: '*'}))
+app.use('/', (req, res) => {
+    res.send('yeyeyeye')
 })
+
+app.listen(process.env.PORT)
+
+
+// mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+//     app.use(cors({origin: '*'}))
+//     app.use(bodyParser.json())
+//     app.use(bodyParser.urlencoded({extended : true}))
+//     app.use('/api', apiRoutes)
+
+
+
+//     app.use('/', (req, res) => {
+//         res.send({"success": false, "message": 'catchall route'})
+//     })
+//     app.listen(PORT)    
+// })

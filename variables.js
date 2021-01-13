e74cf33a2734ca9abc44b2a62a6de9ed6cec836d
Jordan Weatherford <jrw821@gmail.com>
2021-01-13 10:56:34 -0800

    changes

--- variables.js
+++ variables.js
@@ -1,5 +1,5 @@
-exports.DB_URL = process.env.DB_URL
-exports.DOMAIN = process.env.DOMAIN || 'http://localhost:3000'
-exports.PORT = process.env.PORT || 3001
-exports.BUCKET = process.env.BUCKET
-exports.STRIPE_KEY = process.env.STRIPE_KEY
+export const DB_URL = process.env.DB_URL
+export const DOMAIN = process.env.DOMAIN || 'http://localhost:3000'
+export const PORT = process.env.PORT || 3001
+export const BUCKET = process.env.BUCKET
+export const STRIPE_KEY = process.env.STRIPE_KEY
